a5ce5c117427761406763801bff05eb35f8b3956
Paweł Bogaczewicz <0x645@tutanota.com>
2023-11-28 15:26:52 +0100

    feat: dane do device table z zewnątrz

--- mrok/src/app/netsleuth/devices/devices.component.ts
+++ mrok/src/app/netsleuth/devices/devices.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DevicestableComponent } from "../devicetable/devicetable.component";
+import { Device } from '../device';
 
 @Component({
   selector: 'app-devices',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './devices.component.html',
-  styleUrl: './devices.component.css'
+  styleUrl: './devices.component.css',
+  imports: [CommonModule, DevicestableComponent]
 })
 export class DevicesComponent {
-  data = [
+  deviceList: Device[] = [
     { id: 1, name: 'Item 1', ip: '10.20.30.40', location: 'lokalizacja 1', details: 'Details for Item 1' },
     { id: 2, name: 'Item 2', ip: '11.12.13.11', location: 'lokalizacja inna', details: 'Details for Item 2' },
     // Add more data as needed
   ];
-  expandedRowIndex: number | null = null;
 
-  toggleDetails(index: number): void {
-    this.expandedRowIndex = this.expandedRowIndex === index ? null : index;
-  }
+  filteredDeviceList: Device[] = [];
+
 }
